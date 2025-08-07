d3482b90262d3b4c13fffb750b52fe25bdeb70b9
ryukyungwoo <ryukyungwoo1220@gmail.com>
2025-08-07 12:30:28 +0900

    🔥feat: docker compose and make initial script

---
+++ backend/infra/mysql/init/01-create-database.sql
@@ -0,0 +1,8 @@
+DROP database if EXISTS `user_service_db`;
+DROP database if EXISTS `product_service_db`;
+DROP database if EXISTS `order_service_db`;
+
+CREATE database `user_service_db` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE database `product_service_db` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE database `order_service_db` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
