c01e52551471d05e10de6112f662fd0000d5c7a2
ryukyungwoo <ryukyungwoo1220@gmail.com>
2025-08-07 14:41:00 +0900

    🔥 feat: user-service project initailize

---
+++ backend/user-service/src/main/java/com/sesac/userservice/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.sesac.userservice.controller;
+
+import com.sesac.userservice.entity.User;
+import com.sesac.userservice.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/{id}")
+    @Operation(summary = "get user", description = "find user by id")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(userService.findById(id));
+        }  catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
