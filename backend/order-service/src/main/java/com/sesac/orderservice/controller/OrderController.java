2c164345c4f7ba172aad14dc7a9cc49d018c65b8
ryukyungwoo <ryukyungwoo1220@gmail.com>
2025-08-11 11:52:56 +0900

    🔨fix:add requestBody

--- backend/order-service/src/main/java/com/sesac/orderservice/controller/OrderController.java
+++ backend/order-service/src/main/java/com/sesac/orderservice/controller/OrderController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,7 +43,7 @@ public class OrderController {
     }
 
     @PostMapping
-    public ResponseEntity<Order> createOrder(OrderRequestDTO request) {
+    public ResponseEntity<Order> createOrder(@RequestBody OrderRequestDTO request) {
         try {
             Order order = orderService.createOrder(request);
             return ResponseEntity.status(HttpStatus.CREATED).body(order);
