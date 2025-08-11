7781960f8b459aff2a20f472ba3a7e19c35550e8
ryukyungwoo <ryukyungwoo1220@gmail.com>
2025-08-11 14:20:30 +0900

    🔥feat: jwt login feature implement

--- backend/user-service/src/main/java/com/sesac/userservice/controller/UserController.java
+++ backend/user-service/src/main/java/com/sesac/userservice/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.sesac.userservice.controller;
 
+import com.sesac.userservice.dto.LoginRequest;
+import com.sesac.userservice.dto.LoginResponse;
 import com.sesac.userservice.entity.User;
 import com.sesac.userservice.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -8,6 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +29,17 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.OK).body(userService.findById(id));
         }  catch (RuntimeException e) {
             return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/login")
+    @Operation(summary = "login", description = "login with email and password and response jwt token")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
+        try {
+            LoginResponse login = userService.login(request);
+            return ResponseEntity.ok(login);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().build();
         }
     }
 }
